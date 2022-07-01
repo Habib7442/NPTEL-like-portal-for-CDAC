9acadc17b5cd4d2b6c7f497a5e6f4750e0d930f6
Habib Tanwir Laskar <habibtanwir1906@gmail.com>
2022-07-01 16:39:06 +0530

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function changeBorder(){
-    document.getElementById('courses').style.border="1px solid #80a8fc"
-    document.getElementById('instructor').style.border="1px solid #80a8fc"
+function changeBorder() {
+  document.getElementById("courses").style.border = "1px solid #80a8fc";
+  document.getElementById("instructor").style.border = "1px solid #80a8fc";
 }
 
+function getSearch() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  var coursename = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const searchbox = document.getElementById("courses").value;
+
+  fetch(
+    "https://www.backend.cf/courses/:coursename",
+    { mode: "no-cors" },coursename,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result.method);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+}
+
+getSearch();
